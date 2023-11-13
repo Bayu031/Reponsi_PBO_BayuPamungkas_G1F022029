619f0c548285727bee0a8b5874731751fff440f2
Bayu031 <150577523+Bayu031@users.noreply.github.com>
2023-11-13 06:39:54 -0800

    Add files via upload

---
+++ Reponsi_PBO_BayuPamungkas_G1F022029/data/conflict.php
@@ -0,0 +1,26 @@
+<?php
+
+// buat namespace data\satu 
+namespace Data\One {
+    // dengan class conflict
+    class Conflict {
+
+    }
+    // class sample
+    class Sample {
+
+    }
+    // class dummy
+    class Dummy {
+
+    }
+}
+
+// buat namespace data\dua
+namespace Data\Two {
+    // dengan class conflict
+    class Conflict {
+
+    }
+
+}
